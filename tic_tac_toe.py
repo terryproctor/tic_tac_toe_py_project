ef09c58e5ba38443fda014c76731faeb2d88639b
terryproctor <terryproctor2015@outlook.com>
2020-08-21 21:23:27 +0100

    added function to see what squares are free and to check victory no tie situation checked yet (another function to make: check_tie(free) )

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -34,31 +34,74 @@ def DisplayBoard(board):
     print("|   "*3, end = "|\n")
     print("+---"*3, end = "+\n")    
     
-
 DisplayBoard(board)
+
 
 def EnterMove(board):
     #global current_move
     while True:
-        current_move = int(input("Which space would you like to move to?"))
-        if current_move <= 9 and current_move >= 1:
-            board[current_move-1] = "O"
-            DisplayBoard(board)
-            break
+        current_move = input("Which space would you like to move to?")
+        if current_move <= '9' and current_move >= '1':
+            current_move = int(current_move)
+            if board[(current_move-1)] != "O" or "X":
+                board[(current_move-1)] = "O"
+                DisplayBoard(board)
+                break
+        else: 
+            print('try again')
         
-EnterMove(board)
-
 # # the function accepts the board current status, asks the user about their move, 
 # # checks the input and updates the board according to the user's decision
 
 
-# def MakeListOfFreeFields(board):
+def MakeListOfFreeFields(board):
+    free = []
+    for number in range(1,10):
+        if board[number] not in ['O','X']:
+            free.append(number)
+    return free
+
 # #
 # # the function browses the board and builds a list of all the free squares; 
 # # the list consists of tuples, while each tuple is a pair of row and column numbers
 # #
 
-# def VictoryFor(board, sign):
+
+def VictoryFor(board, sign):
+    victory = False
+    global victor
+    victor = None
+    while victory == False:
+        if board[0] == board [1] == board [2]:
+            victor = board[0]
+            victory = True
+        elif board[3] == board [4] == board [5]:
+            victor = board[3]
+            victory = True
+        elif board[6] == board [7] == board [8]:
+            victor = board[6]
+            victory = True
+        elif board[0] == board [3] == board [6]:
+            victor = board[0]
+            victory = True
+        elif board[1] == board [4] == board [7]:
+            victor = board[1]
+            victory = True
+        elif board[2] == board [5] == board [8]:
+            victor = board[2]
+            victory = True
+        elif board[0] == board [4] == board [8]:
+            victor = board[1]
+            victory = True
+        elif board[2] == board [4] == board [6]:
+            victor = board[2]
+            victory = True
+    return victor
+        
+
+
+
+
 # #
 # # the function analyzes the board status in order to check if 
 # # the player using 'O's or 'X's has won the game
